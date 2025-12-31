ec7af83f99e8c20f88165764c76175355a141a90
Fuad Ismail <fuad1502@gmail.com>
2025-12-31 13:07:15 +0700

    oonta: implement function, variable, and binop visitor in IR builder

--- src/ir_builder.rs
+++ src/ir_builder.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 use crate::{
-    ast::{Ast, Expr, FunExpr, LiteralExpr},
-    ir_builder::ir::{Function, IRPrimitive, IRType, IRValue, Instr, Module},
+    ast::{Ast, BinOpExpr, Expr, FunExpr, LiteralExpr, VarExpr},
+    ir_builder::ir::{FunSignature, Function, IRPri, IRType, IRValue, Module},
     lexer::Lexer,
-    typ::{Primitive, Type, TypeMap, Variable, normalize_typ},
+    typ::{Type, TypeMap, normalize_typ},
 };
 
 pub mod ir;
@@ -12,14 +12,14 @@ pub mod ir;
 pub struct IRBuilder<'a> {
     type_map: &'a TypeMap,
     lexer: &'a Lexer,
-    context: Context,
+    context: Option<Context>,
     module: Module,
-    curr_function: String,
+    anon_fun_count: usize,
 }
 
-#[derive(Default)]
 struct Context {
     ir_values: HashMap<String, IRValue>,
+    fun_name: String,
     parent: Option<Box<Context>>,
 }
 
@@ -32,22 +32,21 @@ impl<'a> IRBuilder<'a> {
         Self {
             type_map,
             lexer,
-            context: Context::default(),
+            context: Some(Context::new(ocaml)),
             module,
-            curr_function: ocaml,
+            anon_fun_count: 0,
         }
     }
 
     pub fn build(mut self, ast: &Ast) -> Module {
         for binding in &ast.binds {
             let name = self.lexer.str_from_span(&binding.name).to_string();
-            let ir_typ = self.get_typ(&*binding.expr as *const Expr).unwrap();
-            self.module.new_global_var(name.clone(), ir_typ);
-            let global_val = IRValue::Global(name.clone());
-            self.context.insert(name, global_val.clone());
+            let ir_typ = self.get_ir_typ(&*binding.expr as *const Expr);
+            self.module.new_global_var(name.clone(), ir_typ.clone());
+            let global_val = IRValue::Global(name.clone(), ir_typ);
+            self.insert_name_to_ctx(name, global_val.clone());
             let expr_val = self.visit_expr(&binding.expr);
-            let store_instr = Instr::store(expr_val, global_val);
-            self.curr_function().push_instr(store_instr);
+            self.curr_fun().store(expr_val, global_val);
         }
         self.module
     }
@@ -56,51 +55,207 @@ impl<'a> IRBuilder<'a> {
         let expr_ptr = expr as *const Expr;
         match expr {
             Expr::Literal(literal_expr) => self.visit_literal_expr(literal_expr),
-            Expr::Var(var_expr) => todo!(),
+            Expr::Var(var_expr) => self.visit_var_expr(var_expr),
             Expr::Fun(fun_expr) => self.visit_fun_expr(fun_expr, expr_ptr),
             Expr::Application(application_expr) => todo!(),
             Expr::LetIn(let_in_expr) => todo!(),
-            Expr::BinOp(bin_op_expr) => todo!(),
+            Expr::BinOp(bin_op_expr) => self.visit_bin_op_expr(bin_op_expr, expr_ptr),
         }
     }
 
     fn visit_fun_expr(&mut self, fun_expr: &FunExpr, expr_ptr: *const Expr) -> IRValue {
-        // create function
-        // create closure
-        // bind function to closure's fun
-        // bind environment to closure's env
-        todo!()
+        // 1. Create function
+        let fun_name = self.new_anon_fun_name();
+        let param_names: Vec<String> = fun_expr
+            .params
+            .iter()
+            .map(|p| self.lexer.str_from_span(p).to_string())
+            .collect();
+        let typ = self.get_typ(expr_ptr);
+        let mut fun = Function::from_typ(fun_name.clone(), param_names.clone(), typ);
+        fun.add_param(("env".to_string(), IRType::Ptr));
+
+        // > add function to module
+        self.module.new_function(fun_name.clone(), fun);
+
+        // 2. Populate context
+
+        // > insert parameters to context
+        self.push_ctx(fun_name.clone());
+        for (i, name) in param_names.into_iter().enumerate() {
+            let param = self.curr_fun().param(i);
+            self.insert_name_to_ctx(name, param);
+        }
+
+        // > insert captures to context
+        let num_of_params = self.curr_fun().num_of_params();
+        let env_ptr = self.curr_fun().param(num_of_params - 1);
+        let env_values: Vec<IRValue> = fun_expr
+            .captures
+            .iter()
+            .map(|e| self.get_value_from_ctx(e))
+            .collect();
+        let env_typs: Vec<IRType> = env_values.iter().map(|v| v.typ()).cloned().collect();
+        let env_typ = IRType::Struct(env_typs.clone());
+        for (name, typ) in fun_expr.captures.iter().zip(env_typs) {
+            let ptr = self
+                .curr_fun()
+                .getelemptr(env_typ.clone(), env_ptr.clone(), &[0, 0]);
+            let val = self.curr_fun().load(typ, ptr);
+            self.insert_name_to_ctx(name.to_string(), val);
+        }
+
+        // 3. Create function body
+        let value = self.visit_expr(&fun_expr.body);
+        self.curr_fun().ret(value);
+
+        // 4. Create closure
+        self.pop_ctx();
+        let closure_ptr = self.malloc(4 * (1 + fun_expr.captures.len()));
+        let closure_typ = IRType::Struct(vec![IRType::Ptr, env_typ]);
+
+        // > store anon function ptr
+        let ptr = self
+            .curr_fun()
+            .getelemptr(closure_typ.clone(), closure_ptr.clone(), &[0, 0]);
+        self.curr_fun()
+            .store(IRValue::Global(fun_name, IRType::Ptr), ptr);
+
+        // > store env values
+        for (i, value) in env_values.into_iter().enumerate() {
+            let value = if let IRValue::Global(_, typ) = &value {
+                self.curr_fun().load(typ.clone(), value)
+            } else {
+                value
+            };
+            let ptr = self.curr_fun().getelemptr(
+                closure_typ.clone(),
+                closure_ptr.clone(),
+                &[0, 0, i as i32],
+            );
+            self.curr_fun().store(value, ptr);
+        }
+
+        closure_ptr
+    }
+
+    fn visit_var_expr(&mut self, var_expr: &VarExpr) -> IRValue {
+        let name = self.lexer.str_from_span(&var_expr.id);
+        let val = self.get_value_from_ctx(name);
+        if let IRValue::Global(_, typ) = &val {
+            self.curr_fun().load(typ.clone(), val)
+        } else {
+            val
+        }
+    }
+
+    fn visit_bin_op_expr(&mut self, bin_op_expr: &BinOpExpr, expr_ptr: *const Expr) -> IRValue {
+        let lhs = self.visit_expr(&bin_op_expr.lhs);
+        let rhs = self.visit_expr(&bin_op_expr.rhs);
+        let typ = self.get_ir_typ(expr_ptr);
+        self.curr_fun().add(typ, lhs, rhs)
     }
 
     fn visit_literal_expr(&mut self, literal_expr: &LiteralExpr) -> IRValue {
         match literal_expr {
-            LiteralExpr::Integer(value, _) => IRValue::Primitive(IRPrimitive::Integer(*value)),
+            LiteralExpr::Integer(value, _) => IRValue::Pri(IRPri::I32(*value)),
         }
     }
 
-    fn new_function(fun_expr: &FunExpr) -> Function {
-        todo!()
+    fn get_ir_typ(&self, expr_ptr: *const Expr) -> IRType {
+        IRType::from(self.get_typ(expr_ptr))
     }
 
-    fn get_typ(&self, expr_ptr: *const Expr) -> Option<IRType> {
+    fn get_typ(&self, expr_ptr: *const Expr) -> Type {
         self.type_map
             .get(expr_ptr)
             .map(normalize_typ)
-            .map(|t| match t {
-                Type::Fun(_) => IRType::Ptr,
-                Type::Primitive(Primitive::Integer) => IRType::I32,
-                Type::Variable(Variable::Unbound(_)) => todo!(),
-                Type::Variable(Variable::Link(_)) => unreachable!(),
-            })
+            .expect("Expr not in type_map")
     }
 
-    fn curr_function(&mut self) -> &mut Function {
-        self.module.get_function(&self.curr_function).unwrap()
+    fn curr_fun(&mut self) -> &mut Function {
+        if let Some(context) = &self.context {
+            self.module
+                .get_function(&context.fun_name)
+                .expect("context function not in module")
+        } else {
+            panic!()
+        }
+    }
+
+    fn insert_name_to_ctx(&mut self, name: String, ir_value: IRValue) {
+        if let Some(context) = &mut self.context {
+            context.insert(name, ir_value);
+        } else {
+            panic!("context unassigned")
+        }
+    }
+
+    fn get_value_from_ctx(&self, name: &str) -> IRValue {
+        if let Some(context) = &self.context {
+            context.get(name).expect("name not in context").clone()
+        } else {
+            panic!("context unassigned")
+        }
+    }
+
+    fn push_ctx(&mut self, function_name: String) {
+        let mut new_ctx = Context::new(function_name);
+        new_ctx.parent = self.context.take().map(Box::new);
+        self.context = Some(new_ctx);
+    }
+
+    fn pop_ctx(&mut self) {
+        let parent = self.context.take().map(|c| c.parent);
+        if let Some(Some(parent)) = parent {
+            self.context = Some(*parent);
+        }
+    }
+
+    fn new_anon_fun_name(&mut self) -> String {
+        let name = format!("anon_{}", self.anon_fun_count);
+        self.anon_fun_count += 1;
+        name
+    }
+
+    fn malloc(&mut self, sz: usize) -> IRValue {
+        let sz = IRValue::Pri(IRPri::I64(sz as i64));
+        let ret_typ = match self.module.get_function_decl("malloc") {
+            Some(malloc) => malloc.ret_typ().clone(),
+            None => {
+                let malloc = "malloc".to_string();
+                let ret_typ = IRType::Ptr;
+                let params = vec![IRType::I64];
+                let signature = FunSignature::new(malloc.clone(), ret_typ.clone(), params);
+                self.module.new_function_decl(malloc, signature);
+                ret_typ
+            }
+        };
+        self.curr_fun()
+            .call("malloc".to_string(), ret_typ, vec![sz])
     }
 }
 
 impl Context {
+    fn new(function_name: String) -> Self {
+        Self {
+            ir_values: HashMap::new(),
+            parent: None,
+            fun_name: function_name,
+        }
+    }
+
     fn insert(&mut self, name: String, ir_value: IRValue) {
         self.ir_values.insert(name, ir_value);
+    }
+
+    fn get(&self, name: &str) -> Option<&IRValue> {
+        match self.ir_values.get(name) {
+            Some(val) => Some(val),
+            None => match &self.parent {
+                Some(parent) => parent.get(name),
+                None => None,
+            },
+        }
     }
 }
--- src/ir_builder/ir.rs
+++ src/ir_builder/ir.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
 
+use crate::typ::{Primitive, Type, Variable, normalize_typ};
+
 #[derive(Default)]
 pub struct Module {
     global_vars: Vec<GlobalVar>,
-    functions: HashMap<String, Function>,
+    function_defs: HashMap<String, Function>,
+    function_decls: HashMap<String, FunSignature>,
 }
 
 pub struct GlobalVar {
@@ -19,14 +22,21 @@ pub struct Function {
     used_names: HashMap<String, usize>,
 }
 
+pub struct FunSignature {
+    name: String,
+    ret_typ: IRType,
+    params: Vec<IRType>,
+}
+
 pub struct BasicBlock {
     label: String,
     instrs: Vec<Instr>,
 }
 
+#[derive(Clone)]
 pub struct Instr {
     class: InstrClass,
-    result: IRValue,
+    res: IRValue,
     args: Vec<IRValue>,
 }
 
@@ -34,29 +44,33 @@ pub struct Instr {
 pub enum IRType {
     Void,
     I32,
+    I64,
     Ptr,
     Struct(Vec<IRType>),
 }
 
+#[derive(Clone)]
 pub enum InstrClass {
     Load(IRType),
     Add(IRType),
     Store,
     Call(String),
     GetElemPtr(IRType),
+    Return,
 }
 
 #[derive(Debug, Clone)]
 pub enum IRValue {
     Void,
-    Primitive(IRPrimitive),
-    Register(String, IRType),
-    Global(String),
+    Pri(IRPri),
+    Reg(String, IRType),
+    Global(String, IRType),
 }
 
 #[derive(Debug, Clone)]
-pub enum IRPrimitive {
-    Integer(i32),
+pub enum IRPri {
+    I32(i32),
+    I64(i64),
 }
 
 impl Module {
@@ -66,11 +80,19 @@ impl Module {
     }
 
     pub fn new_function(&mut self, name: String, function: Function) {
-        self.functions.insert(name, function);
+        self.function_defs.insert(name, function);
+    }
+
+    pub fn new_function_decl(&mut self, name: String, signature: FunSignature) {
+        self.function_decls.insert(name, signature);
     }
 
     pub fn get_function(&mut self, name: &str) -> Option<&mut Function> {
-        self.functions.get_mut(name)
+        self.function_defs.get_mut(name)
+    }
+
+    pub fn get_function_decl(&mut self, name: &str) -> Option<&FunSignature> {
+        self.function_decls.get(name)
     }
 }
 
@@ -93,7 +115,107 @@ impl Function {
         fun
     }
 
-    pub fn push_instr(&mut self, instr: Instr) {
+    pub fn from_typ(name: String, param_names: Vec<String>, typ: Type) -> Self {
+        if let Type::Fun(typs) = typ {
+            let mut ir_typs = typs
+                .into_iter()
+                .map(normalize_typ)
+                .map(IRType::from)
+                .collect::<Vec<IRType>>();
+            let ret_typ = ir_typs.pop().unwrap();
+            let params = ir_typs
+                .into_iter()
+                .zip(param_names.into_iter())
+                .map(|(ir_typ, name)| (name, ir_typ))
+                .collect::<Vec<(String, IRType)>>();
+            Function::new(name, ret_typ, params)
+        } else {
+            unreachable!()
+        }
+    }
+
+    pub fn add_param(&mut self, param: (String, IRType)) {
+        let name = self.new_name(&param.0);
+        self.params.push((name, param.1));
+    }
+
+    pub fn param(&self, idx: usize) -> IRValue {
+        IRValue::Reg(self.params[idx].0.clone(), self.params[idx].1.clone())
+    }
+
+    pub fn num_of_params(&self) -> usize {
+        self.params.len()
+    }
+
+    pub fn getelemptr(&mut self, typ: IRType, src: IRValue, indexes: &[i32]) -> IRValue {
+        let mut args = vec![src];
+        let mut indexes = indexes
+            .iter()
+            .map(|i| IRValue::Pri(IRPri::I32(*i)))
+            .collect();
+        args.append(&mut indexes);
+        let res_name = self.new_name("");
+        let instr = Instr {
+            class: InstrClass::GetElemPtr(typ),
+            res: IRValue::Reg(res_name, IRType::Ptr),
+            args,
+        };
+        self.push_instr(instr.clone());
+        instr.value()
+    }
+
+    pub fn load(&mut self, typ: IRType, src: IRValue) -> IRValue {
+        let res_name = self.new_name("");
+        let instr = Instr {
+            class: InstrClass::Load(typ.clone()),
+            res: IRValue::Reg(res_name, typ),
+            args: vec![src],
+        };
+        self.push_instr(instr.clone());
+        instr.value()
+    }
+
+    pub fn store(&mut self, src: IRValue, dst: IRValue) {
+        let instr = Instr {
+            class: InstrClass::Store,
+            res: IRValue::Void,
+            args: vec![src, dst],
+        };
+        self.push_instr(instr);
+    }
+
+    pub fn ret(&mut self, value: IRValue) {
+        let instr = Instr {
+            class: InstrClass::Return,
+            res: IRValue::Void,
+            args: vec![value],
+        };
+        self.push_instr(instr);
+    }
+
+    pub fn call(&mut self, name: String, typ: IRType, args: Vec<IRValue>) -> IRValue {
+        let res_name = self.new_name("");
+        let instr = Instr {
+            class: InstrClass::Call(name),
+            res: IRValue::Reg(res_name, typ),
+            args,
+        };
+        self.push_instr(instr.clone());
+        instr.value()
+    }
+
+    pub fn add(&mut self, typ: IRType, lhs: IRValue, rhs: IRValue) -> IRValue {
+        let res_name = self.new_name("");
+        let instr = Instr {
+            class: InstrClass::Add(typ.clone()),
+            res: IRValue::Reg(res_name, typ),
+            args: vec![lhs, rhs],
+        };
+        self.push_instr(instr.clone());
+        instr.value()
+    }
+
+    fn push_instr(&mut self, instr: Instr) {
         self.bbs.last_mut().unwrap().push_instr(instr);
     }
 
@@ -107,6 +229,20 @@ impl Function {
             self.used_names.insert(name.clone(), 0);
             name
         }
+    }
+}
+
+impl FunSignature {
+    pub fn new(name: String, ret_typ: IRType, params: Vec<IRType>) -> Self {
+        Self {
+            name,
+            ret_typ,
+            params,
+        }
+    }
+
+    pub fn ret_typ(&self) -> &IRType {
+        &self.ret_typ
     }
 }
 
@@ -124,11 +260,30 @@ impl BasicBlock {
 }
 
 impl Instr {
-    pub fn store(src: IRValue, dst: IRValue) -> Self {
-        Self {
-            class: InstrClass::Store,
-            result: IRValue::Void,
-            args: vec![src, dst],
+    pub fn value(self) -> IRValue {
+        self.res
+    }
+}
+
+impl IRValue {
+    pub fn typ(&self) -> &IRType {
+        match self {
+            IRValue::Void => &IRType::Void,
+            IRValue::Pri(IRPri::I32(_)) => &IRType::I32,
+            IRValue::Pri(IRPri::I64(_)) => &IRType::I64,
+            IRValue::Reg(_, ir_type) => ir_type,
+            IRValue::Global(_, ir_type) => ir_type,
+        }
+    }
+}
+
+impl From<Type> for IRType {
+    fn from(typ: Type) -> Self {
+        match typ {
+            Type::Fun(_) => IRType::Ptr,
+            Type::Primitive(Primitive::Integer) => IRType::I32,
+            Type::Variable(Variable::Unbound(_)) => todo!(),
+            Type::Variable(Variable::Link(_)) => panic!(""),
         }
     }
 }
