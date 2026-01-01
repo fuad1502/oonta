60e90b1e9c67d77a3b381fe7fa0e2e2138b78572
Fuad Ismail <fuad1502@gmail.com>
2026-01-01 11:32:19 +0700

    oonta: output test.ll by default and add test driver files to gitignore

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let src_file = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("ocaml")
         .join("main.ml");
-    let out_file = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("main.ll");
+    let out_file = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("test.ll");
     match oonta::compile(&src_file, &out_file) {
         Ok(()) => (),
         Err(e) => eprintln!("{e}"),
