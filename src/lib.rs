eecd943ddbe416fd2cfb70d247f53bd125928446
Fuad Ismail <fuad1502@gmail.com>
2026-01-05 16:12:44 +0700

    skip cargo fmt check on generated files

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,12 @@ pub mod cmd;
 pub mod driver;
 mod ir_builder;
 #[allow(unused)]
+#[rustfmt::skip]
 mod lexer;
+#[rustfmt::skip]
 mod parser;
 #[allow(unused)]
+#[rustfmt::skip]
 mod symbol;
 pub mod terminal_colors;
 mod typ;
