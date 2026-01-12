8669a404085d027495412ebea95c2c2653499b0c
Fuad Ismail <fuad1502@gmail.com>
2026-01-12 17:02:12 +0700

    Update clang-format to a simple rule based on LLVM style

--- runtime/src/gc.hpp
+++ runtime/src/gc.hpp
@@ -4,18 +4,18 @@
 #include <cstddef>
 
 class Gc {
-private:
-  void *heap;
-  size_t heap_size;
-  size_t heap_offset;
+  private:
+    void *heap;
+    size_t heap_size;
+    size_t heap_offset;
 
-  static size_t INITIAL_HEAP_SIZE;
+    static size_t INITIAL_HEAP_SIZE;
 
-  void allocate_new_heap(size_t size);
+    void allocate_new_heap(size_t size);
 
-public:
-  Gc();
-  void *allocate(std::size_t size);
+  public:
+    Gc();
+    void *allocate(std::size_t size);
 };
 
 #endif // GC_H
