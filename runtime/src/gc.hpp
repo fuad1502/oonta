5fc57125fa09e7f0f6f1202da380c5dfb2c706d0
Fuad Ismail <fuad1502@gmail.com>
2026-08-29 15:08:47 +0700

    Implement generational heap & allocation

--- runtime/src/gc.hpp
+++ runtime/src/gc.hpp
@@ -1,19 +1,36 @@
 #ifndef GC_H
 #define GC_H
 
+#include <unordered_map>
+
 #define UNW_LOCAL_ONLY
 #include "libunwind.h"
-#include <cstddef>
 
-class Gc {
+class Heap {
   private:
     void *heap;
     size_t heap_size;
     size_t heap_offset;
 
-    static size_t INITIAL_HEAP_SIZE;
+    size_t header_size_from_pointer_field_offs(size_t *pointer_field_offs,
+                                               size_t pointer_field_offs_len);
+    void write_header(void *obj_ptr, size_t *pointer_field_offs,
+                      size_t pointer_field_offs_len);
 
-    void allocate_new_heap(size_t size);
+  public:
+    Heap(size_t size);
+    void *allocate(size_t size, size_t *pointer_field_offs,
+                   size_t pointer_field_offs_len);
+};
+
+class Gc {
+  private:
+    static size_t GEN0_HEAP_SIZE;
+    static size_t GEN1_INITIAL_HEAP_SIZE;
+
+    Heap *gen0_heap;
+    Heap *gen1_heap;
+    std::unordered_map<unw_word_t, struct Safepoint *> *safepoints_map;
 
   public:
     Gc();
