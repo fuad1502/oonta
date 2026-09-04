2580c0651e11844775ca67807a33d553f39e8ef8
Fuad Ismail <fuad1502@gmail.com>
2026-09-04 18:23:47 +0700

    Fix next gen collection & add safepoint call on failed allocation

--- runtime/src/gc_extern.cpp
+++ runtime/src/gc_extern.cpp
@@ -9,6 +9,19 @@ char gcneedcollection = 0;
 extern "C" void *gcmalloc(size_t size, size_t *type_info) {
     void *ptr = gc.allocate(type_info);
 
+    if (ptr == nullptr) {
+        unw_cursor_t cursor;
+        unw_context_t uc;
+
+        unw_getcontext(&uc);
+        unw_init_local(&cursor, &uc);
+        unw_step(&cursor);
+
+        gc.safepoint(cursor);
+
+        ptr = gc.allocate(type_info);
+    }
+
     if (gc.need_collection()) {
         gcneedcollection = 1;
     }
@@ -24,7 +37,7 @@ extern "C" void gcsafepoint() {
     unw_init_local(&cursor, &uc);
     unw_step(&cursor);
 
-    gc.safepoint(&cursor);
+    gc.safepoint(cursor);
 
     gcneedcollection = 0;
 }
